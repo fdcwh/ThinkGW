4d3741a02da3ef47ca6ab8656181ba9861904a30
曹伟华 <1291713293@qq.com>
2022-07-20 00:19:06 +0800

    登录页面

--- app/admin/middleware/Init.php
+++ app/admin/middleware/Init.php
@@ -22,7 +22,46 @@ class Init {
 	 * @return mixed
 	 */
 	public function handle($request, \Closure $next) {
+		$this->setMenuData($request);
 		return $next($request);
 	}
 
+	/**
+	 * @title setMenuData
+	 *
+	 * @param $request
+	 */
+	private function setMenuData($request) {
+		$controller             = $request->controller();
+		$initData['top_middle'] = '';
+		//
+		$initData['_dialog']        = $request->param('_dialog', 0);
+		$initData['_staticVersion'] = date('Ymd');
+		// 读取菜单信息 ajax 以及部分控制器不需要执行
+		if (!$initData['_dialog'] && !$request->isAjax()) {
+			$initData['_leftMenu']    = self::getMenu();
+			$initData['_getLocation'] = [];
+			// 选中当前
+			$currentIds             = array_values(array_column($initData['_getLocation'], 'id', 'id'));
+			$initData['_current_0'] = $currentIds['0'] ?? 0;
+			$initData['_current_1'] = $currentIds['1'] ?? 0;
+		}
+		View::assign('_initData', $initData);
+	}
+
+	/**
+	 * @title  getMenu
+	 * @return
+	 */
+	private static function getMenu() {
+		return [
+			[
+				'id'        => 1,
+				'title'     => '控制台',
+				'url_value' => '/admin/index/index',
+				'icon'      => '',
+				'url_type'  => 0,
+			]
+		];
+	}
 }
