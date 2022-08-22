868e8e0f446996b1b4340e9ab67493c68193c7f2
曹伟华 <1291713293@qq.com>
2022-08-23 00:38:25 +0800

    页面

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -3,13 +3,34 @@
 namespace app\index\controller;
 
 use app\BaseController;
+use think\facade\View;
 
 class Index extends BaseController {
 	public function index() {
-		return 'ThinkIM';
+		$vars = [];
+		return View::fetch('index', $vars);
 	}
 
-	public function hello($name = 'ThinkPHP6') {
-		return 'hello,' . $name;
+	public function to() {
+		// 指明给谁推送，为空表示向所有在线用户推送
+		$to_uid = '';
+		// 推送的url地址，上线时改成自己的服务器地址
+		$push_api_url = "http://0.0.0.0:2121/";
+		$post_data    = [
+			'type'    => 'publish',
+			'content' => '这个是推送的测试数据',
+			'to'      => $to_uid,
+		];
+		$ch           = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $push_api_url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ["Expect:"]);
+		$return = curl_exec($ch);
+		curl_close($ch);
+		var_export($return);
+		exit('ok');
 	}
 }
